9deee89051e0328c943a77248dd49475a31f1681
bibek Tamang <bibeklama123asd@gmail.com>
2026-09-24 10:05:53 +0545

    feat:implement routes fro seller dashboard

--- backend/src/routes/dashboard.routes.ts
+++ backend/src/routes/dashboard.routes.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import * as dashboardController from "../controllers/dashboard.controller.js";
 import protect from "../middleware/auth.middleware.js";
-import adminOnly from "../middleware/role.middleware.js";
+import adminOnly, { sellerOnly } from "../middleware/role.middleware.js";
 
 const router = express.Router();
 
@@ -25,5 +25,41 @@ router.get(
   dashboardController.getLowStockProducts,
 );
 router.get("/sales", protect, adminOnly, dashboardController.getSalesOverview);
+
+//seller
+
+router.get(
+  "/seller/stats",
+  protect,
+  sellerOnly,
+  dashboardController.getSellerDashboardStats,
+);
+router.get(
+  "/seller/recent-orders",
+  protect,
+  sellerOnly,
+  dashboardController.getSellerRecentOrders,
+);
+
+router.get(
+  "/seller/top-products",
+  protect,
+  sellerOnly,
+  dashboardController.getSellerTopSellingProducts,
+);
+
+router.get(
+  "/seller/sales-overview",
+  protect,
+  sellerOnly,
+  dashboardController.getSellerSalesOverview,
+);
+
+router.get(
+  "/seller/low-stock",
+  protect,
+  sellerOnly,
+  dashboardController.getSellerLowStockProducts,
+);
 
 export default router;
